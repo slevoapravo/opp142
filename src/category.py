f2243e97916bfd6cee7325720af6297960f3e0c3
Your Name <you@example.com>
2025-07-04 16:46:14 +0300

    2

--- src/category.py
+++ src/category.py
@@ -15,10 +15,9 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.__products = products
+        self.__products = products if isinstance(products, list) else []
         Category.category_count += 1
-        Category.product_count += len(products)
-        print(Category.product_count)
+        Category.product_count += len(self.__products)
 
     def add_product(self, product: dict):
         """Метод добавления нового продукта в список"""
@@ -26,7 +25,7 @@ class Category:
             self.__products.append(product)
             Category.product_count += 1
         else:
-            raise TypeError
+            raise TypeError(f"Expected an instance of Product, got {type(product).__name__} instead.")
 
     @property
     def get_product_list(self) -> str:
@@ -36,5 +35,5 @@ class Category:
         return product_list
 
 
-# result = Category("Product", "Description", ["product1", "product2", "product3"])
-# print(result)
+result = Category("Product", "Description", ["product1", "product2", "product3"])
+print(result)

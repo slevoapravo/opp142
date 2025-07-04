dbcbf163cef09b1e37bcfb11b4a3edc8a5291e39
Your Name <you@example.com>
2025-07-04 14:11:38 +0300

    1

---
+++ src/category.py
@@ -0,0 +1,40 @@
+
+from typing import Any
+from src.product import Product
+
+class Category:
+    """Категория товара"""
+
+    category_count = 0
+    product_count = 0
+
+    name: str
+    description: str
+    products: list
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.__products = products
+        Category.category_count += 1
+        Category.product_count += len(products)
+        print(Category.product_count)
+
+    def add_product(self, product: dict):
+        """Метод добавления нового продукта в список"""
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise TypeError
+
+    @property
+    def get_product_list(self) -> str:
+        product_list = ""
+        for product in self.__products:
+            product_list += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
+        return product_list
+
+
+# result = Category("Product", "Description", ["product1", "product2", "product3"])
+# print(result)
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+
+import json
+import os
+from typing import Any
+
+from src.category import Category
+from src.product import Product
+
+
+def read_json_file(path: str) -> Any:
+    """Конвертирует файл json в словарь"""
+    full_path = os.path.abspath(path)
+    with open(full_path, "r", encoding="UTF-8") as file:
+        data = json.load(file)
+    return data
+
+
+result = read_json_file("..//data/products.json")
+
+
+def create_object_from_json(data: dict) -> Any:
+    """Преобразует данные из словаря в объекты класса"""
+    categories = []
+    for category in data:
+        products = []
+        for product in category["products"]:
+            products.append(Product(**product))
+            name = category["name"]
+            description = category["description"]
+            # price = category["price"]
+            # quantity = category["quantity"]
+            category_instance = Category(name, description, products)
+            categories.append(category_instance)
+        return categories
+
+
+result_2 = create_object_from_json(result)
+print(result_2)
+
